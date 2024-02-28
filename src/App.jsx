4a1d72f0c05d81e2f5896a3d0a7d3483ae967b11
Basit <160310683+basitnexisoft@users.noreply.github.com>
2024-02-28 21:57:59 +0500

    update28

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from './components/Home'
 import About from './components/About'
 import Services from './components/Services'
 import Doctors from './components/Doctors'
+import Footer from './components/Footer'
+
 function App() {
 
   return (
@@ -30,6 +32,7 @@ function App() {
           Blog
         </div>
       </main>
+      <Footer/>
       
     </>
   )
--- src/layouts/BlogCard.jsx
+++ src/layouts/BlogCard.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const BlogCard = ({img , headlines}) => {
   return (
     <div className="w-full lg:w-1/4 p-2 shadow-[rgba(0,0,0,0.24) 0px 3px 8px] space-y-2 rounded-lg cursor-pointer hover:scale-105 transition duration -300 ease-in-out">
-        <img src={img} alt="img" />
-        <h1>{headlines}</h1>
-        <p>
+        <img className="h-64 md:h-96 lg:h-40 w-full rounded-lg" 
+        src={img} 
+        alt="img" 
+        />
+        <h1 className="text-lg text-center font-semibold">{headlines}</h1>
+        <p className="text-center text-sm">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.Distinctio
           dolor nobis pariatur provident aut rem deserunt architecto masiores,
           aliquam dignissimos.
